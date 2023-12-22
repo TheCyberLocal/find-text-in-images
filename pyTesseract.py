e95b44a3e78863d8fb7b4ebab2e9212d0536f7a4
Timothy Macfarlane <thecyberlocal@gmail.com>
2023-12-22 15:34:12 -0600

    Delete pyTesseract.py

--- pyTesseract.py
+++
@@ -1,9 +0,0 @@
-import pytesseract as tess
-
-tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-from PIL import Image
-
-img = Image.open('pic_text.png')
-text = tess.image_to_string(img)
-
-print(text)
